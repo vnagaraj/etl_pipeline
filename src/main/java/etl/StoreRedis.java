30eada6044066068079e70c189bc13652e57b6f9
Vivek Nagarajan <vivekanandganapathynagarajan@Vivekanands-MacBook-Pro.local>
2017-02-12 05:55:45 -0800

    Preprocessing step: Store in Redis the keys(fileName) and value(userInfo)

---
+++ src/main/java/etl/StoreRedis.java
@@ -0,0 +1,55 @@
+package etl;
+
+import org.apache.log4j.Logger;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import util.AWSUtil;
+import util.ReadYaml;
+import util.FileInfo;
+import redis.clients.jedis.Jedis;
+
+import java.io.File;
+
+
+/**
+ * Preprocessing step of ETL Pipeline:
+ * Store in Redis(key:fileName, value:userInfo)
+ *
+ * @author Vivekanand Ganapathy Nagarajan
+ * @version 1.0 Feb 5th, 2017
+ */
+public class StoreRedis {
+    private static Logger logger = Logger.getLogger(StoreRedis.class);
+
+    public static void main(String[] args) {
+        AWSUtil.configureLog();
+        if (args.length < 1){
+            logger.error("userconfig dir not specified");
+            System.exit(-1);
+        }
+        run(args[0]);
+    }
+
+    public static void run(String fileDir){
+        File[] files = new File(fileDir).listFiles();
+        if (files == null){
+            logger.error("Listing of files failed");
+            System.exit(-1);
+        }
+        logger.info("Storing in Redis");
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), AWSUtil.redis_master);
+        Jedis jedis = pool.getResource();
+        for (File file: files) {
+            if (!file.getName().endsWith("yaml")){
+                continue;
+            }
+            //Path p = Paths.get(file.getName());
+            FileInfo fileInfo =  ReadYaml.readYaml(file.getPath());
+            String fileName = fileInfo.getInput();
+            logger.info("Storing key "+ fileName + " value " + file.getName());
+            //store data in redis list
+            jedis.set(fileName, file.getName());
+        }
+
+    }
+}
