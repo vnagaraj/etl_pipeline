66865b252a4eccfb31934bf2981043004c48e8cd
Ubuntu <ubuntu@ip-172-31-0-4.us-west-2.compute.internal>
2017-02-15 02:12:21 +0000

    Fixed airflow test script

--- airflow/airflowtest.sh
+++ airflow/airflowtest.sh
@@ -8,16 +8,18 @@ echo "Cleaned up airlfow logs in airflow worker-2"
 rm -rf ~/efs/etl_pipeline/tmp/e*
 echo "Cleaned up airlfow logs in airflow master"
 #delete files from input/output bucket in S3
-cd .. && mvn exec:java -Dexec.mainClass=etl.DeleteFiles
+mvn exec:java -Dexec.mainClass=etl.CleanupFiles
 # generate one file
-python ~/efs/etl_pipeline/scripts/file_generator.py
+python scripts/file_generator.py
 #Upload file to S3
 mvn exec:java -Dexec.mainClass=etl.UploadFiles -Dexec.args=files
 #Yaml generator
-python ~/efs/etl_pipeline/scripts/yaml_generator.py
+python scripts/yaml_generator.py
+#pipeline generator
+python scripts/pipeline_gen.py
 #Store Redis
 mvn exec:java -Dexec.mainClass=etl.StoreRedis -Dexec.args=userconfig
 #airflow test tasks
-airflow test readMessageFromQueue etl1 02-14-2017
-airflow test readRedis etl1
-airflow test flinkTransform etl1
+airflow test etl1 readMessageFromQueue  02-14-2017
+airflow test etl1 readRedis 02-14-2017
+airflow test etl1 flinkTransform 02-14-2017
